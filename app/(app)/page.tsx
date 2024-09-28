39f44d81461a62e8136d66de76aff46ad8b8f2a0
Mohnish Gorana <mohnishgorana1@gmail.com>
2024-09-28 13:57:25 +0530

    initiated home page

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import HomepageButtonControls from "@/components/HomePageButtonControls";
 import { fetchProfileAction } from "@/lib/actions/profile.action";
 import { useUser } from "@clerk/nextjs";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
+import Loading from "../Loading";
 
 function Home() {
   const router = useRouter();
@@ -24,18 +27,52 @@ function Home() {
   }, [isLoaded, user]);
 
   if (isloading || !isLoaded) {
-    return <div>Loading...</div>; // Show loading state while fetching profile info
+    return <div><Loading /></div>; // Show loading state while fetching profile info
   }
 
   if (!profileInfo) {
-    router.push('/onboard')
+    router.push("/onboard");
     return null; // Prevent rendering while redirecting
   }
 
   return (
-    <section className="">
-      <h1>Home</h1>
-    </section>
+    <Fragment>
+      <section className="relative w-full h-full min-h-screen pb-10">
+        <div className="w-full h-full relative">
+          <div className="flex flex-col-reverse lg:flex-row gap-10 mt-16">
+            <section className="w-full lg:w-[50%] flex flex-col md:px-2 lg:px-0 p-5 lg:p-10">
+              <div className="w-full flex justify-start flex-col h-auto lg:pt-7">
+                <span className="flex space-x-2">
+                  <span className="block w-14 mb-2 dark:border-white border-b-2 border-gray-700"></span>
+                  <span className="font-medium dark:text-white text-gray-600">
+                    One Stop Solution to Find Jobs
+                  </span>
+                </span>
+                <h1 className="text-3xl dark:text-white mt-5 lg:text-7xl text-black font-extrabold">
+                  Build your best job community starting from here.
+                </h1>
+                <div className="w-full mt-6 flex items-center text-white justify-start gap-2">
+                  <HomepageButtonControls
+                    user={JSON.parse(JSON.stringify(user))}
+                    profileInfo={profileInfo}
+                  />
+                </div>
+              </div>
+            </section>
+            <section className="relative w-full lg:w-[50%] flex items-center justify-end">
+              <Image
+                // src="https://utfs.io/f/4c9f7186-8ad0-4680-aece-a5abea608705-k6t10e.png"
+                src={"/assets/images/recruiter-hero.jpg"}
+                alt="Hero"
+                className="h-full w-full "
+                width={50}
+                height={50}
+              />
+            </section>
+          </div>
+        </div>
+      </section>
+    </Fragment>
   );
 }
 
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+      },
+      // {
+      //   protocol: "https",
+      //   hostname: "sleek-capybara-771.convex.cloud",
+      // },
+    ],
+  },
+};
 
 export default nextConfig;
